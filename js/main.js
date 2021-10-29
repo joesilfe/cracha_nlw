529c54b2dba44337528e12f685fcc50442dc4a40
Joel Silva <joel.silva.ferr@gmail.com>
2021-10-29 17:52:59 -0300

    Crachá sendo desenvolvido no NLW

---
+++ js/main.js
@@ -0,0 +1,48 @@
+const linkSocialMedia = {
+    github: {
+        user: "joesilfe",
+    },
+    youtube: {
+        user: "",
+    },
+    facebook: {
+        user: "joel.silve.149",
+    },
+    instagram: {
+        user: "joesilfe",
+    },
+    twitter: {
+        user: "",
+    },
+};
+
+function changeSocialMedia() {
+    for (li of socialLinks.children) {
+        const social = li.getAttribute("class");
+
+        li.children[0].href = `https://${social}.com/${linkSocialMedia[social].user}`;
+    }
+
+    // userName.textContent = "Marcelo"
+}
+
+function getGitHubProfileInfos() {
+
+    const url = `https://api.github.com/users/${linkSocialMedia.github.user}`;
+
+    fetch(url)
+    .then((resp) => resp.json())
+    .then((data) => {
+        userName.textContent = data.name
+        userBio.textContent = data.bio
+        userLink.href = data.html_url
+        userImage.src = data.avatar_url
+        userLogin.textContent = data.login
+
+        console.log(data)
+    })
+    .catch((err) => console.log(err));
+}
+
+changeSocialMedia();
+getGitHubProfileInfos();
